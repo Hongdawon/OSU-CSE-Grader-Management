7f7ae7517172100cabb58cb43f16ee3da0a11c9b
Isaac Cooper <isaaccooper10@gmail.com>
2024-10-14 12:05:42 -0400

    custom devise views for choosing role on sign up and editing password only

--- grader_management_system/app/controllers/application_controller.rb
+++ grader_management_system/app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   # Ensure the user is logged in before accessing any pages
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:role, :email, :password, :password_confirmation)}
+
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:role, :email, :password, :current_password, :password_confirmation)}
+    end
+
   private
 
   # Method to check if the current user is an admin
